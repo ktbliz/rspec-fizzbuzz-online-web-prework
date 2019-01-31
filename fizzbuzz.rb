58167852574215168a99a7fb99b2c01a56b1a3a3
Katie Blizzard <kblizzard11@gmail.com>
2019-01-31 22:08:15 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,11 +3,11 @@
 
 def fizzbuzz(int)
 
-  if int % 3 && int % 5
+  if int % 3 == 0 && int % 5 == 0
     fizzbuzz
-  elsif int % 3
+  elsif int % 3 == 0
     fizz
-  elsif int % 5
+  elsif int % 5 == 0 
     buzz 
   end 
 
