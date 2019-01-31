7575997f2794a4afedf66e8e7fae4e0baac35bcc
Katie Blizzard <kblizzard11@gmail.com>
2019-01-31 22:08:57 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,14 +1,14 @@
-# Don't forget! This file needs to be 'required' in its spec file
+# Don't forget! This file needs to be 'reFquired' in its spec file
 # See README.md for instructions on how to do this
 
 def fizzbuzz(int)
 
   if int % 3 == 0 && int % 5 == 0
-    fizzbuzz
+    "FizzBuzz"
   elsif int % 3 == 0
-    fizz
+    "Fizz"
   elsif int % 5 == 0 
-    buzz 
+    "Buzz" 
   end 
 
 end
